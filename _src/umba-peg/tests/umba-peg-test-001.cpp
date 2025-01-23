d8e5ecf558fdd228783a03d47f989f2085afb0e5
Alexandr Martynov <mag@unicont.com>
2025-01-23 16:00:16 +0300

    Запилил пустой тест

---
+++ _src/umba-peg/tests/umba-peg-test-001.cpp
@@ -0,0 +1,23 @@
+/*! \file
+    \brief 
+ */
+
+// Должна быть первой
+#include "umba/umba.h"
+//---
+#include "umba/app_main.h"
+#include "umba/debug_helpers.h"
+#include "umba/string_plus.h"
+#include "umba/rule_of_five.h"
+
+//
+#include "umba/tokenizer.h"
+
+
+
+UMBA_APP_MAIN()
+{
+    UMBA_USED(argc); UMBA_USED(argv);
+
+    return 0;
+}
